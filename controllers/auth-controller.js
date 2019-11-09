4aefcf618c6484ddc15bfe2e21839219eb8adeaf
Matthew Perez <mep10@rice.edu>
2019-11-09 13:58:17 -0600

    more authentication

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -5,3 +5,18 @@ var jwt = require('jsonwebtoken');
 var request = require('request');
 var xmlParser = require('xml2js').parseString;
 var stripPrefix = require('xml2js').processors.stripPrefix;
+
+var config = require('../scr/config');
+
+var User = require('../src/model/chema/User');
+
+router.use(bodyParser.json());
+
+router.get('/', function (req, res) {
+	var ticket = req.query.ticket;
+	var casValidateURL = 'https://idp.rice.edu/idp/profile/cas/serviceValidate';
+
+	if (ticket){
+		var url = casValidateURL;
+	}
+})
