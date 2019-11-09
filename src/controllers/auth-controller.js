8d585516fd6edfd3031d795d50b3494c448cfcbd
Matthew Perez <mep10@rice.edu>
2019-11-09 14:09:46 -0600

    URL for post-authentication makes sense now

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -6,17 +6,19 @@ var request = require('request');
 var xmlParser = require('xml2js').parseString;
 var stripPrefix = require('xml2js').processors.stripPrefix;
 
-var config = require('../scr/config');
+var config = require('../config');
 
-var User = require('../src/model/chema/User');
+var User = require('../model/schema/User');
 
 router.use(bodyParser.json());
 
 router.get('/', function (req, res) {
 	var ticket = req.query.ticket;
-	var casValidateURL = 'https://idp.rice.edu/idp/profile/cas/serviceValidate';
-
 	if (ticket){
-		var url = casValidateURL;
+		var casValidateURL = 'https://idp.rice.edu/idp/profile/cas/serviceValidate';
+		// This should be the URL that we redirect the user back to after successful CAS authentication
+		var serviceURL = 'http://localhost:3000/auth'; // Using local host for testing purposes
+		
+		var url = ${casValidateURL}?ticket=${ticket}?service=?{serviceURL};
 	}
 })
