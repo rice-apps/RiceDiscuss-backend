1a98a55af5b56cd13fefad9a347bdd34d9073c3f
David Yi <davidcyyi@gmail.com>
2019-11-23 12:25:19 -0600

    changed schema for mongodb for votes

--- src/model/schema/Post.js
+++ src/model/schema/Post.js
@@ -10,13 +10,8 @@ var PostSchema = new Schema({
 	title: { type: String, required: true },
 	body: {type: String, required: true },
 	date_created: {type: Date, default: Date.now()},
-	votes: {
-		type: {
-			user_id: { type: Schema.Types.ObjectId, required: true },
-			vote: { type: Number, enum: [1, 0, -1], required: true }
-		},
-		required: true //Might need to verify if it works might need default
-	},
+	upvotes: [{type: Schema.Types.ObjectId, required: true, ref: 'User'}],
+	downvotes: [{type: Schema.Types.ObjectId, required: true, ref: 'User'}],
 	tags: [ { type: String } ],
 	type: { type: String, required: true, default: "discussion", 
 			enum:["discussion", "job", "event"] },
