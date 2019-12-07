39fb4a531368045c8096227a942496d15d842288
Victor Song <vsong1618@gmail.com>
2019-12-07 14:33:47 -0600

    Update Query schema to reflect implemented resolvers for posts, comments, and users

--- src/graphql/schema/Query.js
+++ src/graphql/schema/Query.js
@@ -2,8 +2,11 @@ import gql from 'apollo-server-express';
 
 const QueryGQLSchema = gql`
     type Query {
+        posts: [Post]
         post(_id: ID): Post
+        users: [User]
         user(_id: ID): User
+        comments: [Comment]
         comment(_id: ID): Comment
     }
 `;
