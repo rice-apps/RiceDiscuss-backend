2376cfdf9cf1992b019065a12fbaa148f9028fe5
Victor Song <vms2@rice.edu>
2020-05-18 03:41:54 -0500

    Formatted more stuff

--- src/compose/Comment.js
+++ src/compose/Comment.js
@@ -5,24 +5,29 @@ import { schemaComposer } from 'graphql-compose';
 const CommentSchema = mongoose.Schema({
         creator: {
             type: String,
-            required: true,
+            required: true
         },
+
         post_id: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Post"
         },
+
         parent_id: {
             type: Schema.Types.ObjectId,
             ref: "Comment"
         },
+
         date_created: {
             type: Date,
             required: true
         },
+
         body: {
             type: String,
             required: true
         },
+        
         upvotes: {
             type: [{
                 type: mongoose.Schema.Types.ObjectId,
@@ -56,7 +61,7 @@ const CommentSchema = mongoose.Schema({
             type: Number,
             required: true,
             default: 0
-        },
+        }
 });
 
 const Comment = mongoose.model('Comment', CommentSchema);
