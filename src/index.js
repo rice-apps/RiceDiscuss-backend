c1b45c1b263c3da4f83be96ab685fddb1eb957f7
Victor Song <vms2@rice.edu>
2020-05-19 13:38:47 -0500

    Added websockets

--- src/index.js
+++ src/index.js
@@ -23,6 +23,6 @@ const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
 
 httpServer.listen({ port: PORT }, () => {
-    console.log(`*** === Server ready at http://localhost:${PORT}${server.graphqlPath} === ***`);
-    console.log(`*** === Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath} === ***`);
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath} !`);
+    console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath} !`);
 });
