3d2f516302726507a724d0d1fb2656bd86d0bacb
Victor Song <vms2@rice.edu>
2020-05-19 21:04:27 -0500

    Minor change to index.js logging

--- src/index.js
+++ src/index.js
@@ -23,6 +23,6 @@ const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
 
 httpServer.listen({ port: PORT }, () => {
-    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath} !`);
-    console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath} !`);
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}!`);
+    console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}!`);
 });
