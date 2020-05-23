8aa9a206fdbce02d19434c26710e3efe1b5dea1d
Victor Song <vms2@rice.edu>
2020-05-22 21:51:34 -0500

    Added token checking in context for Apollo Server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import http from 'http';
+import jwt from 'jsonwebtoken';
 
 import Schema from './schema';
 import oAuth from './controllers/auth-controller';
 
 import './db';
 
-import { DEV_PORT } from './config';
+import {
+    CLIENT_TOKEN_SECRET,
+    DEV_PORT
+} from './config';
 
 const server = new ApolloServer({
     schema: Schema,
+    context: (req, res) => {
+        const token = req.token | req.headers.authorization;
+        var decoded = null;
+
+        try {
+            decoded = jwt.verify(token, CLIENT_TOKEN_SECRET);
+        } catch {
+            return res.status(403);
+        }
+    
+        return {
+            netID: decoded.data.user,
+        };
+    },
     subscriptions: {
         onConnect: (connectionParams, webSocket, context) => {
             console.log("WebSocket connected!");
@@ -20,9 +38,6 @@ const server = new ApolloServer({
             console.log("WebSocket disconnected!");
         },
     },
-    context: {
-        
-    }
 });
 
 const app = express();
