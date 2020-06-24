a108feec5b5179390383595fdbe65bc1aa4d626a
Victor Song <vms2@rice.edu>
2020-06-24 03:29:52 -0500

    Added .env.example

--- src/config.js
+++ src/config.js
@@ -8,9 +8,9 @@ const SERVICE_URL = process.env.SERVICE_URL;
 const DEV_PORT = parseInt(process.env.DEV_PORT);
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS.split(",");
 const CAS_VALIDATE_URL = process.env.CAS_VALIDATE_URL;
-const COLLEGES = process.env.COLLEGES.split(",");
-const MAJORS = process.env.MAJORS.split(",");
-const MINORS = process.env.MINORS.split(",");
+const COLLEGES = process.env.COLLEGES.split(";");
+const MAJORS = process.env.MAJORS.split(";");
+const MINORS = process.env.MINORS.split(";");
 
 const PAGINATION_OPTIONS = {
     paginationResolverName: "pagination", // Default
