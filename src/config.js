7bf6b8469fa964fb69b2aa35587fd2d38f468809
Victor Song <vms2@rice.edu>
2020-07-06 18:15:23 -0500

    Sorted variable declaration in config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,15 @@
 import "dotenv/config";
 import sanitizeHtml from "sanitize-html";
 
-const CLIENT_TOKEN_SECRET = process.env.CLIENT_TOKEN_SECRET;
-const TOKEN_EXPIRE_TIME = parseInt(process.env.TOKEN_EXPIRE_TIME);
-const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
-const SERVICE_URL = process.env.SERVICE_URL;
-const DEV_PORT = parseInt(process.env.DEV_PORT);
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS.split(",");
+const DEV_PORT = parseInt(process.env.DEV_PORT);
+const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+
 const CAS_VALIDATE_URL = process.env.CAS_VALIDATE_URL;
+const CLIENT_TOKEN_SECRET = process.env.CLIENT_TOKEN_SECRET;
+const SERVICE_URL = process.env.SERVICE_URL;
+const TOKEN_EXPIRE_TIME = parseInt(process.env.TOKEN_EXPIRE_TIME);
+
 const COLLEGES = process.env.COLLEGES.split(";");
 const MAJORS = process.env.MAJORS.split(";");
 const MINORS = process.env.MINORS.split(";");
@@ -27,13 +29,13 @@ const MONGOOSE_CONFIG = {
 };
 
 export {
-    CLIENT_TOKEN_SECRET,
-    TOKEN_EXPIRE_TIME,
-    MONGODB_CONNECTION_URL,
-    SERVICE_URL,
-    DEV_PORT,
     ALLOWED_ORIGINS,
+    DEV_PORT,
+    MONGODB_CONNECTION_URL,
     CAS_VALIDATE_URL,
+    CLIENT_TOKEN_SECRET,
+    SERVICE_URL,
+    TOKEN_EXPIRE_TIME,
     COLLEGES,
     MAJORS,
     MINORS,
